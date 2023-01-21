b61b1725403a65b1f4da71bb78a85886d26abb23
Igor Malinavichus <crazych4tty@gmail.com>
2023-01-21 09:28:17 +0300

    fix: выполнить множественные правки в коде

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 let editButton = document.querySelector(".button_type_edit");
 let closeButton = document.querySelector(".button_type_close");
-let submitButton = document.querySelector(".button_type_submit");
+
 let popup = document.querySelector(".popup");
 let editForm = document.querySelector(".edit-form");
 let nameInput = document.querySelector(".edit-form__input_type_name");
 let jobInput = document.querySelector(".edit-form__input_type_job");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let nameText;
+let jobText;
 
 function openPopup() {
   popup.classList.add("popup_opened");
-  let profileName = document.querySelector(".profile__name").textContent;
-  let profileJob = document.querySelector(".profile__job").textContent;
-  nameInput.value = profileName;
-  jobInput.value = profileJob;
+  nameText = profileName.textContent;
+  jobText = profileJob.textContent;
+  nameInput.value = nameText;
+  jobInput.value = jobText;
 }
 
 editButton.addEventListener("click", openPopup);
@@ -24,8 +28,6 @@ closeButton.addEventListener("click", closePopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__job");
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   popup.classList.remove("popup_opened");
