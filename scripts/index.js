8d85446809d22e58118a82f9f9244550ac8f8600
Igor Malinavichus <crazych4tty@gmail.com>
2023-01-23 03:16:06 +0300

    fix: Убрать заданную высоту в headewr и убрать лишние переменные из js кода

--- scripts/index.js
+++ scripts/index.js
@@ -7,15 +7,11 @@ let nameInput = document.querySelector(".edit-form__input_type_name");
 let jobInput = document.querySelector(".edit-form__input_type_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
-let nameText;
-let jobText;
 
 function openPopup() {
   popup.classList.add("popup_opened");
-  nameText = profileName.textContent;
-  jobText = profileJob.textContent;
-  nameInput.value = nameText;
-  jobInput.value = jobText;
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 editButton.addEventListener("click", openPopup);
