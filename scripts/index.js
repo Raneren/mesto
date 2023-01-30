9c1ba184e004b7c4032e07437e64400320a910ea
Igor Malinavichus <crazych4tty@gmail.com>
2023-01-30 22:20:58 +0300

    feat: Сделать поп-ап добавления карточки нового места

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,50 @@
-let editButton = document.querySelector(".button_type_edit");
-let closeButton = document.querySelector(".button_type_close");
+const editButton = document.querySelector(".button_type_edit");
+const addButton = document.querySelector(".button_type_add");
+const closeButtonPopupEdit = document.querySelector(
+  ".button_type_close-popup-edit"
+);
+const closeButtonPopupAdd = document.querySelector(
+  ".button_type_close-popup-add"
+);
 
-let popup = document.querySelector(".popup");
+const popups = document.querySelectorAll(".popup");
+const popupEdit = document.querySelector(".popup_type_edit-profile-info");
+const popupAdd = document.querySelector(".popup_type_add-place-card");
+
 let editForm = document.querySelector(".edit-form");
 let nameInput = document.querySelector(".edit-form__input_type_name");
 let jobInput = document.querySelector(".edit-form__input_type_job");
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 
-function openPopup() {
-  popup.classList.add("popup_opened");
+function openPopupEdit() {
+  popupEdit.classList.add("popup_opened");
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-editButton.addEventListener("click", openPopup);
+editButton.addEventListener("click", openPopupEdit);
 
-function closePopup() {
-  popup.classList.remove("popup_opened");
+function openPopupAdd() {
+  popupAdd.classList.add("popup_opened");
 }
 
-closeButton.addEventListener("click", closePopup);
+addButton.addEventListener("click", openPopupAdd);
+
+function closePopup() {
+  popups.forEach(function (popup) {
+    popup.classList.remove("popup_opened");
+  });
+}
+
+closeButtonPopupEdit.addEventListener("click", closePopup);
+closeButtonPopupAdd.addEventListener("click", closePopup);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popup.classList.remove("popup_opened");
+  closePopup();
 }
 
 editForm.addEventListener("submit", handleFormSubmit);
